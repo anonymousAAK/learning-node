62163375604ddbd6b7ab42c666dc9e1640beaadb
anonymousAAK <71527203+anonymousAAK@users.noreply.github.com>
2022-11-27 19:10:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+
+//CLI
+
+const amount = 11
+
+if (amount < 10) {
+  console.log('small number')
+} else {
+  console.log('large number')
+}
+
+console.log(`hey it's my first node app!!!`)
